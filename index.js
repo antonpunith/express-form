e507e4b46040ecb4a6d01c5b3d71c9032345db3c
Anton Punith <antonpunith@gmail.com>
2018-04-08 10:05:55 +1000

    handling post and get

---
+++ index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var url = require('url');
+var app = express();
+var bodyParser = require('body-parser')
+
+app.get('/', function (req, res) {
+  res.send(`
+    <form action="login" method="POST">
+      <input name="username"/>
+      <button type="submit">Submit</button>
+    </form>
+  `);
+
+  var url_parts = url.parse(req.url, true);
+  console.log(url_parts.query);
+});
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+// Handles json data
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+// Handles form data
+
+// POST /login gets urlencoded bodies
+app.post('/login', urlencodedParser, function (req, res) {
+  if (!req.body) return res.sendStatus(400)
+  console.log(req.body);
+  res.send('welcome, ' + req.body.username)
+})
+
+// POST /api/users gets JSON bodies
+app.post('/api/users', jsonParser, function (req, res) {
+
+  if (!req.body) return res.sendStatus(400)
+  console.log(req.body);
+  res.send('welcome')
+})
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
