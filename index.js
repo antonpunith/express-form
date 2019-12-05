10564016f7968310e77eb9d31837cd805e5895f5
Anton Punith <anton.punith@monash.edu>
2019-12-05 11:50:39 +1100

    cookie parser

--- index.js
+++ index.js
@@ -2,17 +2,26 @@ var express = require('express');
 var url = require('url');
 var app = express();
 var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+
+app.use(cookieParser());
 
 app.get('/', function (req, res) {
+  var cookie = req.cookies.testCookie;
+  console.log('cookies', req.cookies);
+  if (cookie === undefined)
+  {
+    res.cookie("testCookie", "Hello", { maxAge: 900000, httpOnly: true });
+
+  }
   res.send(`
     <form action="login" method="POST">
       <input name="username"/>
       <button type="submit">Submit</button>
     </form>
   `);
-
   var url_parts = url.parse(req.url, true);
-  console.log(url_parts.query);
+  console.log('url params', url_parts.query);
 });
 
 // create application/json parser
